391b3f618262d157183bed94db78b8128f24d566
Carl Ellhar Holmberg <carhol@student.chalmers.se>
2021-09-28 12:27:12 +0200

    Implemented scaling and full vectorization

--- prototypes/hand_tracking/HandTrackingBaseChange.py
+++ prototypes/hand_tracking/HandTrackingBaseChange.py
@@ -3,6 +3,7 @@ import mediapipe as mp
 import time
 import numpy as np
 from numpy.core.numeric import zeros_like
+from mediapipe.python.solutions.hands import HandLandmark
 
 cap = cv2.VideoCapture(0)
 
@@ -13,88 +14,129 @@ hands = mpHands.Hands(static_image_mode=False,
                       min_tracking_confidence=0.5)
 mpDraw = mp.solutions.drawing_utils
 
+# Parameters and constants
 pTime = 0
 cTime = 0
+n_nodes = 21
 
-hand_coords = [np.zeros(2) for _ in range(21)]
-hand_screen_coords = np.zeros_like(hand_coords)
-scaled_screen_coords = np.zeros_like(hand_coords)
+# Data structures
+pinky_val = 0.
+ring_val = 0.
+middle_val = 0.
+index_val = 0.
+thumb_val = 0.
+hand_nodes = np.ones((3,21))
 
-def set_coordinates(id, val) :
-    hand_coords[id] = np.array([val.x * w, val.y * h])
-    #np.matmul(screen_mat, hand_coords[id], hand_screen_coords[id])
+# Vectorized functions
+def lms_to_vectors(nodes):
+    for i, val in enumerate(nodes):
+        hand_nodes[:,i] = np.array([val.x * w, val.y * h, 1])
 
-def translate(vec):
-    for i, coord in enumerate(hand_coords):
-        hand_coords[i] = coord + vec
+def translate_vectors(vec):
+    trans_mat = np.array([[1, 0, vec[0]],[0, 1, vec[1]],[0, 0, 1]])
+    np.matmul(trans_mat, hand_nodes, hand_nodes)
 
-def get_rot_matrix(angle):
-    return np.array(
-        [[np.cos(angle), -np.sin(angle)],
-        [np.sin(angle), np.cos(angle)]])
+def rotate_vectors(angle):
+    rot_mat = np.array(
+        [[np.cos(angle), -np.sin(angle), 0],
+        [np.sin(angle), np.cos(angle), 0],
+        [0, 0, 1]])
+    np.matmul(rot_mat, hand_nodes, hand_nodes)
 
-def rotate(angle):
-    mat = get_rot_matrix(-angle)
-    for i, coord in enumerate(hand_coords):
-        np.matmul(mat, coord, hand_coords[i])
+def normalize_vectors(draw_length):
+    base_length = hand_nodes[0,HandLandmark.MIDDLE_FINGER_MCP]
+    scale_factor = draw_length/base_length
+    scaling_mat = np.array([
+        [scale_factor,0,0],
+        [0,scale_factor,0],
+        [0,0,1]])
+    np.matmul(scaling_mat, hand_nodes, hand_nodes)
 
-def normalize_all(draw_length: int):
-    base_length = hand_coords[middle_knuckle_index][0]
-    for i, coord in enumerate(hand_coords):
-        hand_coords[i] = coord * (draw_length / base_length)
-
+# ===== MAIN LOOP ======
 while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    #imgblank = np.zeros(img.shape, np.uint8)
-    imgblank = np.zeros(img.shape, np.uint8)
+    img_canvas = np.zeros(img.shape, np.uint8)
     results = hands.process(imgRGB)
     
-    h, w, c = imgblank.shape
+    h, w, c = img_canvas.shape
     
     base_offset = np.array([0, h // 2])
-    wrist_index = 0
-    middle_knuckle_index = 9
-
+    base_offset_vec = np.array([0, h // 2, 1])
+    clr = (255,255,255)
     base_vec_draw_length = 300
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
-
             # Convert to vectors
-            for id, lm in enumerate(handLms.landmark):
-                set_coordinates(id, lm)
+            lms_to_vectors(handLms.landmark)
 
             # Get the wrist vector
-            wrist_pos = hand_coords[wrist_index]
+            wrist_pos_vec = hand_nodes[:,HandLandmark.WRIST]
 
             # Set the wrist position as origo
-            translate(-wrist_pos)
+            translate_vectors(-wrist_pos_vec)
 
             # Get the angle of the wrist-to-middle-knucle vector
-            knuckle_pos = hand_coords[middle_knuckle_index]
-            angle = np.arctan2(knuckle_pos[1],knuckle_pos[0])
+            knuckle_pos_vec = hand_nodes[:,HandLandmark.MIDDLE_FINGER_MCP]
+            angle_vec = np.arctan2(knuckle_pos_vec[1],knuckle_pos_vec[0])
             
             # Rotate all nodes around origo
-            rotate(angle)
+            rotate_vectors(-angle_vec)
 
             # Normalize all
-            normalize_all(base_vec_draw_length)
+            normalize_vectors(base_vec_draw_length)
 
             # Get the screen coordinates
-            translate(base_offset)
-            
-            for vec in hand_coords:
-                clr = (255,0,255) if id == 1 else (255,255,255)
-                cv2.circle(imgblank, (int(vec[0]), int(vec[1])), 3, clr, cv2.FILLED)
+            translate_vectors(base_offset_vec)
 
-            cv2.line(imgblank, hand_coords[wrist_index].astype(int), hand_coords[middle_knuckle_index].astype(int), (255,0,0), 4)
-            
+            # Calculate rough values
+            pinky_val = np.linalg.norm(hand_nodes[:,HandLandmark.PINKY_TIP] - hand_nodes[:,HandLandmark.WRIST]) / base_vec_draw_length
+            ring_val = np.linalg.norm(hand_nodes[:,HandLandmark.RING_FINGER_TIP] - hand_nodes[:,HandLandmark.WRIST])  / base_vec_draw_length
+            middle_val = np.linalg.norm(hand_nodes[:,HandLandmark.MIDDLE_FINGER_TIP] - hand_nodes[:,HandLandmark.WRIST])  / base_vec_draw_length
+            index_val = np.linalg.norm(hand_nodes[:,HandLandmark.INDEX_FINGER_TIP] - hand_nodes[:,HandLandmark.WRIST])  / base_vec_draw_length
+            thumb_val = np.linalg.norm(hand_nodes[:,HandLandmark.THUMB_TIP] - hand_nodes[:,HandLandmark.WRIST])  / base_vec_draw_length
+
+            # Draw on the original image
+            for id, lm in enumerate(handLms.landmark):
+                cx, cy = int(lm.x *w), int(lm.y*h)
+                cv2.circle(img, (cx,cy), 3, (255,0,255), cv2.FILLED)
+
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+            # Draw points and base line
+            for i in range(n_nodes):
+                vec = hand_nodes[:,i]
+                cv2.circle(img_canvas, (int(vec[0]), int(vec[1])), 3, clr, cv2.FILLED)
+
+            cv2.line(img_canvas, 
+                hand_nodes[0:2,HandLandmark.WRIST].astype(int), 
+                hand_nodes[0:2,HandLandmark.MIDDLE_FINGER_MCP].astype(int), 
+                (255,0,0), 4)
+
+    # Display performance
     cTime = time.time()
     fps = 1/(cTime-pTime)
     pTime = cTime
+    
+    # Display values
+    v_offset = h // 12
+    cv2.putText(img_canvas,str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+    cv2.putText(img_canvas,f"Pinky: {pinky_val}", (7 * w//10, h // 10), cv2.FONT_HERSHEY_PLAIN, 2, (255,0,255), 3)
+    cv2.putText(img_canvas,f"Ring: {ring_val}", (7 * w//10, 2 * h // 10), cv2.FONT_HERSHEY_PLAIN, 2, (255,0,255), 3)
+    cv2.putText(img_canvas,f"Middle: {middle_val}", (7 * w//10, 3 * h // 10), cv2.FONT_HERSHEY_PLAIN, 2, (255,0,255), 3)
+    cv2.putText(img_canvas,f"Index: {index_val}", (7 * w//10, 4 * h // 10), cv2.FONT_HERSHEY_PLAIN, 2, (255,0,255), 3)
+    cv2.putText(img_canvas,f"Thumb: {thumb_val}", (7 * w//10, 5 * h // 10), cv2.FONT_HERSHEY_PLAIN, 2, (255,0,255), 3)
 
-    cv2.putText(imgblank,str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+    # Overlay the camera image on the canvas
+    img_scale_factor = 4
+    scaled_width = w // img_scale_factor
+    scaled_height = h // img_scale_factor
+    scaled_img = cv2.resize(img, (scaled_width, scaled_height))
+    imgpos_x = w - scaled_width
+    imgpos_y = h - scaled_height
+    img_canvas[imgpos_y:imgpos_y+scaled_img.shape[0],imgpos_x:imgpos_x+scaled_img.shape[1]] = scaled_img
 
-    cv2.imshow("Image", imgblank)
+    # Show the complete canvas
+    cv2.imshow("Image", img_canvas)
     cv2.waitKey(1)
